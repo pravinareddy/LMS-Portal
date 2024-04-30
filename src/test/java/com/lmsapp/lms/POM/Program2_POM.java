08b366a0ae9d3a950a1be86fd960e3e590a45ba1
15146 <15146@DESKTOP-0KH341F.home>
2024-04-30 15:08:07 -0400

    intial commit

--- src/test/java/com/lmsapp/lms/POM/Program2_POM.java
+++ src/test/java/com/lmsapp/lms/POM/Program2_POM.java
@@ -10,12 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-
 import io.cucumber.java.lu.a;
 
 public class Program2_POM {
