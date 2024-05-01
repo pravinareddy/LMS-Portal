aa1b1e32b6672fcbc368b1213c5b8f05fca45bc0
jaykrishi <jrameshan@gmail.com>
2024-04-30 22:01:13 -0500

    my second commit

--- src/test/java/com/lmsapp/lms/stepDefinition/CommonMethodsStepDefinition.java
+++ src/test/java/com/lmsapp/lms/stepDefinition/CommonMethodsStepDefinition.java
@@ -3,9 +3,12 @@ package com.lmsapp.lms.stepDefinition;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
